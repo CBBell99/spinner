de5edfac4e3876df62af8d09e638c5e5b29ddd41
Chris Bell <chrisbeckertonbell@gmail.com>
2022-01-18 10:04:42 -0800

    initial commit, refactor of spinner1.js

---
+++ spinner2.js
@@ -0,0 +1,44 @@
+const string = '|/-\\|/-\\|'
+count = 100
+
+for (const char of string) {
+  setTimeout(() => {
+    process.stdout.write(`\r${char}   `)
+  }, count += 200)
+}
+
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 100);
+
+// setTimeout(() => {
+//   process.stdout.write('\r/   ');
+// }, 300);
+
+// setTimeout(() => {
+//   process.stdout.write('\r-   ');
+// }, 500);
+
+// setTimeout(() => {
+//   process.stdout.write('\r\\   ');
+// }, 700);
+
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 900);
+
+// setTimeout(() => {
+//   process.stdout.write('\r/  ');
+// }, 1100);
+
+// setTimeout(() => {
+//   process.stdout.write('\r-  ');
+// }, 1300);
+
+// setTimeout(() => {
+//   process.stdout.write('\r\\  ');
+// }, 1500);
+
+// setTimeout(() => {
+//   process.stdout.write('\r|  ');
+// }, 1700);
